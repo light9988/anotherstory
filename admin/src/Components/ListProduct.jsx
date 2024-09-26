bd8676b38cbd0ac7db4e4a74faa5059fde245fb9
Jia <jiajia@annamacbook.lan>
2024-09-25 22:39:18 -0700

    update code

--- admin/src/Components/ListProduct.jsx
+++ admin/src/Components/ListProduct.jsx
@@ -32,71 +32,81 @@ const ListProduct = () => {
         await fetchInfo();
     }
 
-
     return (
-        <div className="flex flex-col items-center w-[80%]">
-            <h1 className="text-3xl font-bold p-5">All Product List</h1>
-            <div className=" grid grid-cols-8 font-semibold gap-1 md:gap-2 lg:gap-10 py-1  ">
-                <p className='flex justify-center'>Products</p>
-                <p className='col-start-2 col-span-3 flex justify-center '>Title</p>
-                {/* <p className='flex justify-center' >Quantity</p> */}
-                <p className='flex justify-center' >Old Price</p>
-                <p className='flex justify-center '>New Price</p>
-                <p className='flex justify-center '>Category</p>
-                <p className='flex justify-center'>Remove</p>
+        <div className="list-product-container">
+            <div className="list-product-header">
+                <h1 className="list-product-header-title">All Product List</h1>
+                <div className="list-product-header-menu">
+                    <p className='list-product-header-menu-list'>Products</p>
+                    <p className='list-product-header-menu-list' id="list-product-header-menu-list-title">Title</p>
+                    <p className='list-product-header-menu-list'>Old Price</p>
+                    <p className='list-product-header-menu-list'>New Price</p>
+                    <p className='list-product-header-menu-list'>Category</p>
+                    <p className='list-product-header-menu-list'>Remove</p>
+                </div>
+
+                <div className="list-product-body">
+                    {allProducts.map((product, i) => {
+                        return (
+                            <div key={i} className="list-product-item">
+                                <img src={product.image} className="list-product-image" alt="" />
+                                <p className='list-product-column' id="list-product-column-name">{product.name}</p>
+                                <p className='list-product-column'>${product.old_price}</p>
+                                <p className='list-product-column'>${product.new_price}</p>
+                                <p className='list-product-column'>${product.category}</p>
+                                <img
+                                    className="list-product-column"
+                                    id="list-product-remove"
+                                    src={remove_icon}
+                                    alt=""
+                                    onClick={() => { remove_product(product.id) }}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
 
-            <div className="my-5 py-5  bg-gray-50 overflow-y-scroll h-1/3">
-                {allProducts.map((product, i) => {
-                    return <div key={i} className="items-center border-b-2 gap-1 md:gap-2 lg:gap-10
-                border-gray-400 py-2 grid grid-cols-8  ">
-                        <img src={product.image} width={80} height={80} alt="" className=" mx-auto" />
-                        <p className='flex justify-center col-start-2 col-span-3 '>{product.name}</p>
-                        {/* <p className='flex justify-center  '>{product.quantity}</p> */}
-                        <p className='flex justify-center '>${product.old_price}</p>
-                        <p className='flex justify-center '>${product.new_price}</p>
-                        <p className='flex justify -center'>${product.category}</p>
-                        <img className="cursor-pointer  mx-auto " width={10} height={10} src={remove_icon} alt="" onClick={() => { remove_product(product.id) }} />
-                    </div>
-                })}
-            </div>
-
+            {/* <div className="list-product-footer">
+                <h2 className="list-product-footer-content">More</h2>
+            </div> */}
         </div>
-    )
+    );
 }
 
-export default ListProduct
+export default ListProduct;
 
-// return (
-//     <div className="list-product-container">
-//         {/* <div className="list-product-header"> */}
-//         <h2 className="list-product-header-title">All Product List</h2>
-//         <div className="list-product-header-menu">
-//             <p className="list-product-column">Products</p>
-//             <p className="list-product-column">Title</p>
-//             <p className='flex justify-center' >Quantity</p>
-//             <p className="list-product-column">Old Price</p>
-//             <p className="list-product-column">New Price</p>
-//             <p className="list-product-column">Category</p>
-//             <p className="list-product-column">Remove</p>
+
+//     return (
+//         <div className="list-product-header flex flex-col items-center w-[80%]">
+//             <h1 className="list-product-header-title text-3xl font-bold p-5">All Product List</h1>
+//             <div className="list-product-header-menu grid grid-cols-8 font-semibold gap-1 md:gap-2 lg:gap-10 py-1  ">
+//                 <p className='list-product-header-menu-list flex justify-center'>Products</p>
+//                 <p className='list-product-header-menu-list col-start-2 col-span-3 flex justify-center ' id="list-product-header-menu-list-title">Title</p>
+//                 {/* <p className='flex justify-center' >Quantity</p> */}
+//                 <p className='list-product-header-menu-list flex justify-center' >Old Price</p>
+//                 <p className='list-product-header-menu-list flex justify-center '>New Price</p>
+//                 <p className='list-product-header-menu-list flex justify-center '>Category</p>
+//                 <p className='list-product-header-menu-list flex justify-center'>Remove</p>
+//             </div>
+
+//             <div className="list-product-body my-5 py-5  bg-gray-50 overflow-y-scroll h-1/3">
+//                 {allProducts.map((product, i) => {
+//                     return <div key={i} className="list-product-item items-center border-b-2 gap-1 md:gap-2 lg:gap-10
+//                 border-gray-400 py-2 grid grid-cols-8  ">
+//                         <img src={product.image} width={80} height={80} alt="" className="list-product-image mx-auto" />
+//                         <p className='list-product-column flex justify-center col-start-2 col-span-3 '>{product.name}</p>
+//                         {/* <p className='flex justify-center  '>{product.quantity}</p> */}
+//                         <p className='list-product-column flex justify-center '>${product.old_price}</p>
+//                         <p className='list-product-column flex justify-center '>${product.new_price}</p>
+//                         <p className='list-product-column flex justify -center'>${product.category}</p>
+//                         <img className="list-product-column cursor-pointer  mx-auto " id="list-product-remove" width={10} height={10} src={remove_icon} alt="" onClick={() => { remove_product(product.id) }} />
+//                     </div>
+//                 })}
+//             </div>
+
 //         </div>
-//         {/* </div> */}
-    
-//         <div className="list-product-body">
-//             {allProducts.map((product, i) => {
-//                 return <div key={i} className="list-product-item">
-//                     <img className="list-product-image" src={product.image} alt="" />
-//                     <p className="list-product-column">{product.name}</p>
-//                     <p className='flex justify-center  '>{product.quantity}</p>
-//                     <p className="list-product-column">${product.old_price}</p>
-//                     <p className="list-product-column">${product.new_price}</p>
-//                     <p className="list-product-column">${product.category}</p>
-//                     <img className="list-product-column" id="list-product-remove" src={remove_icon} alt="" onClick={() => { remove_product(product.id) }} />
-//                 </div>
-//             })}
-//         </div>
-//     </div>
-// )
+//     )
 // }
 
 // export default ListProduct
--- admin/src/Components/Sidebar.jsx
+++ admin/src/Components/Sidebar.jsx
@@ -2,6 +2,28 @@ import {Link} from 'react-router-dom';
 import add_product_icon from '../assets/Product_Cart.svg';
 import list_product_icon from '../assets/Product_list_icon.svg';
 import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+      <div className="sidebar">
+          <Link to={'/addproduct'} className='sidebar-link'>
+              <div className="sidebar-item">
+                  <img src={add_product_icon} alt="" />
+                  <p>Add Product</p>
+              </div>
+          </Link>
+          <Link to={'/listproduct'} className='sidebar-link'>
+              <div className="sidebar-item">
+                  <img src={list_product_icon} alt="" />
+                  <p>List Product</p>
+              </div>
+          </Link>
+      </div>
+    )
+  }
+  
+export default Sidebar
+  
 // const Sidebar = () => {
 //   return (
 //     <div className="fle flex-col pt-7   w-[100%] max-w-[250px] h-screen bg-black">
@@ -23,23 +45,3 @@ import './Sidebar.css'
 
 // export default Sidebar
 
-const Sidebar = () => {
-    return (
-      <div className="sidebar">
-          <Link to={'/addproduct'} className='sidebar-link'>
-              <div className="sidebar-item">
-                  <img src={add_product_icon} alt="" />
-                  <p>Add Product</p>
-              </div>
-          </Link>
-          <Link to={'/listproduct'} className='sidebar-link'>
-              <div className="sidebar-item">
-                  <img src={list_product_icon} alt="" />
-                  <p>List Product</p>
-              </div>
-          </Link>
-      </div>
-    )
-  }
-  
-  export default Sidebar
